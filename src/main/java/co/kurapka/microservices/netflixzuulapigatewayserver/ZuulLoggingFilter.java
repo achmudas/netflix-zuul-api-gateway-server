6a7071dceceb918e13a4d115b9f79edd8035afe5
Remigijus <achmudass@gmail.com>
2019-11-17 16:53:21 +0200

    Introducing API gateway

---
+++ src/main/java/co/kurapka/microservices/netflixzuulapigatewayserver/ZuulLoggingFilter.java
@@ -0,0 +1,40 @@
+package co.kurapka.microservices.netflixzuulapigatewayserver;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+
+import com.netflix.zuul.ZuulFilter;
+import com.netflix.zuul.context.RequestContext;
+import com.netflix.zuul.exception.ZuulException;
+
+@Component
+public class ZuulLoggingFilter extends ZuulFilter {
+
+	private static final Logger logger = LoggerFactory.getLogger(ZuulLoggingFilter.class);
+
+	@Override
+	public boolean shouldFilter() {
+		return true;
+	}
+
+	@Override
+	public Object run() throws ZuulException {
+		HttpServletRequest req = RequestContext.getCurrentContext().getRequest();
+		logger.info("Request -> {}, request uri -> {}", req, req.getRequestURI());
+		return req;
+	}
+
+	@Override
+	public String filterType() {
+		return "pre";
+	}
+
+	@Override
+	public int filterOrder() {
+		return 1;
+	}
+
+}
